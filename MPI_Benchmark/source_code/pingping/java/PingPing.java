fa3f8c98a532a62ee9523d7f14d28160b162dd69
Emiliano Firmino <emiliano.firmino@gmail.com>
2014-10-16 08:43:28 -0300

    Java pingping ran in parallel, but still not finish
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

--- MPI_Benchmark/source_code/pingping/java/PingPing.java
+++ MPI_Benchmark/source_code/pingping/java/PingPing.java
@@ -1,76 +1,79 @@
+import java.util.concurrent.Semaphore;
+
 public class PingPing extends Thread{
+    private Semaphore semaphore;
 
-	private final int QTD_PROC_TOTAL = 2;
-	private int qtdProcTerminados = 0;
-	
-	private long timeSpawn;
-	private long timeExec;
+    private int qtdProcTotal;
+    private int qtdProcTerminados = 0;
+    
+    private long timeSpawn;
+    private long timeExec;
 
-	public long timeStart;
-	public long timeEnd;
+    public long timeStart;
+    public long timeEnd;
 
-	private int tamDados;
-	private int qtdRept;
-	private int pairsN;
-	private String outLocation;
+    private int tamDados;
+    private int qtdRept;
+    private int pairsN;
+    private String outLocation;
 
-	public PingPing(int tamDados, int qtdMsg, int pairsN, String outLocation) {
-		this.tamDados = tamDados;
-		this.qtdRept = qtdMsg;
-		this.pairsN = pairsN;
-		this.outLocation = outLocation;
-	}
+    public PingPing(int tamDados, int qtdMsg, int pairsN, String outLocation) {
+        this.tamDados = tamDados;
+        this.qtdRept = qtdMsg;
+        this.pairsN = pairsN;
+        this.outLocation = outLocation;
+        this.semaphore = new Semaphore(-(2 * pairsN) + 1);
+    }
 
-	public void run() {
-		byte[] dado = generateData(tamDados);
+    public void run() {
+        byte[] dado = generateData(tamDados);
+        ProcPing procs[] = new ProcPing[2 * pairsN];
+        timeStart = timeMicroSeg();
+        
+        timeStart = timeMicroSeg();
+        for (int i = 0; i < procs.length; i+= 2) {
+            procs[i] = new ProcPing("" + i, dado, this, qtdRept);
+            procs[i + 1] = new ProcPing("" + (i + 1), dado, this, qtdRept);
+        }
+        timeEnd = timeMicroSeg();
+        timeSpawn = timeEnd - timeStart;
 
-		//timeStart = timeMicroSeg();
-		for (int pair = 0; pair < pairsN; pair++) {
-			System.out.println(pair);
-			ProcPing p1 = new ProcPing("1", dado, this, qtdRept);
-			ProcPing p2 = new ProcPing("2", dado, this, qtdRept);
-			//timeEnd = timeMicroSeg();
+        timeStart = timeMicroSeg();
+        for (int i = 0; i < procs.length; i+= 2) {
+            procs[i].setPeer(procs[i+1]);
+            procs[i+1].setPeer(procs[i]);
 
-			//timeSpawn = timeEnd - timeStart;
+            procs[i].start();
+            procs[i+1].start();
+        }
+        dormirAteTerminar();
+        timeEnd = timeMicroSeg();
+        
+        timeExec = timeEnd - timeStart;
+        Salvar.writeResultPeer(outLocation, tamDados, qtdRept, timeExec, timeSpawn);
+    }
 
-			//timeStart = timeMicroSeg();
-			p1.setPeer(p2);
-			p1.start();
-			p2.setPeer(p1);
-			p2.start();
+    private synchronized void dormirAteTerminar() {
+        try {
+            this.semaphore.acquire();
+        } catch (InterruptedException ie)
+        {
+        }
+        System.out.printf("" + qtdProcTerminados);
+    }
 
-			dormirAteTerminar();
+    public synchronized void acordar() {
+        qtdProcTerminados++;
+        System.out.printf("-" + java.lang.Thread.currentThread().getName());
+        this.semaphore.release();
+    }
 
-			//timeEnd = timeMicroSeg();
+    private byte[] generateData(int tamDados) {
+        byte[] dado = new byte[tamDados];
+        return dado;
+    }
 
-			//timeExec = timeEnd - timeStart;
-		}
-		//timeExec = timeMicroSeg();
-		//timeSpawn = timeMicroSeg();
-		//Salvar.writeResultPeer(outLocation, tamDados, qtdRept, timeExec, timeSpawn);
-	}
-
-	private synchronized void dormirAteTerminar() {
-		while(qtdProcTerminados!= QTD_PROC_TOTAL){
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public synchronized void acordar() {
-		qtdProcTerminados++;
-		notifyAll();
-	}
-
-	private byte[] generateData(int tamDados) {
-		byte[] dado = new byte[tamDados];
-		return dado;
-	}
-
-	private long timeMicroSeg() {
-		return System.nanoTime()/1000;
-	}
+    private long timeMicroSeg() {
+        return System.nanoTime()/1000;
+    }
 }
